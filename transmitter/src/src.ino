62561ac8fee345d4153837d7a169bb010c8feb75
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-03-31 22:38:56 +0300

    Add DHT sensor functionality to transmitter code

--- transmitter/src/src.ino
+++ transmitter/src/src.ino
@@ -1,24 +1,43 @@
+#include <SimpleDHT.h>
 #include "data-controller.h"
 
-const DataController *dataController = new DataController();
+#define PIN_DHT 2
+
+DataController *dataController = new DataController();
+SimpleDHT11 dht(PIN_DHT);
+
+byte dhtTemp = 0;
+byte dhtHumid = 0;
+
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   while (!Serial.availableForWrite())
   {
     delay(100);
   }
+  Serial.println("SR_RDY");
 }
 
 void loop()
 {
   if (Serial.availableForWrite() > 0)
   {
-    float humidity = 3.4567;
-    char *name = "HUM";
+    getDHTSensorValues();
+    dataController->sendData("TMP", dhtTemp);
+    delay(1000);
+    dataController->sendData("HUM", dhtHumid);
+    delay(1000);
+  }
+}
 
-    dataController->sendData(name, humidity);
+void getDHTSensorValues() {
+  int err = SimpleDHTErrSuccess;
+    if ((err = dht.read(&dhtTemp, &dhtHumid, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("DHT Error="); Serial.print(SimpleDHTErrCode(err));
+    Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(2000);
+    return;
   }
 }
