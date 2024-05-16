2a8a06bd42e4096d6c08c9dd3be91e61b7d9df99
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-05-16 19:03:41 +0300

    Update local server to optimize requests

--- local-server/app/sensor-data.controller.ts
+++ local-server/app/sensor-data.controller.ts
@@ -1,23 +1,54 @@
-import { SensorData } from './types';
+import { SensorData, TemporaryDatabase } from './types';
+
+const temporaryDatabase: TemporaryDatabase = {
+  data: [],
+};
 
 export async function postSensorData(data: string, uri: string): Promise<void> {
-  const sensorData = data.toString().split(':');
-  if (sensorData.length !== 2) return console.error('Invalid data format:', sensorData);
+  const sensorData = data.toString().split('\r\n');
+  if (sensorData.length < 2) return console.error('Invalid data format:', sensorData);
 
-  const sensorObject: SensorData = {
-    sensorId: sensorData[0],
-    value: Number.parseFloat(sensorData[1]),
+  pushToTemporaryDatabase(sensorData);
+
+  if (temporaryDatabase.data.length > 100) {
+    await fetch(`${uri}/sensor-data`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(temporaryDatabase),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error: Error) => console.error(error));
+
+    cleanTemporaryDatabase();
+  }
+}
+
+function pushToTemporaryDatabase(sensorData: string[]): void {
+  const dataChunk: SensorData[] = [];
+  if (temporaryDatabase.dateStart === undefined) {
+    temporaryDatabase.dateStart = new Date(Date.now());
+  }
+
+  for (let index = 0; index < sensorData.length; index += 2) {
+    dataChunk.push({
+      sensorName: sensorData[index].trim(),
+      value: Number.parseFloat(sensorData[index + 1].trim()),
+    });
+  }
+
+  temporaryDatabase.data.push({
     date: new Date(),
-  };
+    sensorData: dataChunk,
+  });
 
-  await fetch(`${uri}/sensor-data`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(sensorObject),
-  })
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error: Error) => console.error(error));
+  temporaryDatabase.dateEnd = new Date(Date.now());
+}
+
+function cleanTemporaryDatabase(): void {
+  temporaryDatabase.data = [];
+  temporaryDatabase.dateStart = undefined;
+  temporaryDatabase.dateEnd = undefined;
 }
--- local-server/app/types.ts
+++ local-server/app/types.ts
@@ -1,5 +1,21 @@
 export interface SensorData {
-  sensorId: string;
+  sensorName: string;
   value: number;
+}
+
+export interface DataObject {
+  sensorData: SensorData[];
+  dateStart: Date;
+  dateEnd: Date;
+}
+
+export interface TemporaryDatabase {
+  dateStart?: Date;
+  dateEnd?: Date;
+  data: TemporaryDataChunk[];
+}
+
+export interface TemporaryDataChunk {
   date: Date;
+  sensorData: SensorData[];
 }
