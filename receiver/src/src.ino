095cf67faa5a4f88c54c3d195494cac09111cd17
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-05-24 22:18:14 +0300

    Reciever code changes reverted

--- receiver/src/src.ino
+++ receiver/src/src.ino
@@ -1,27 +1,54 @@
-#include "communication_handler.h"
-#include "adaptive_light.h"
-
 #define LDR_PIN A0
-#define LAMBDA 5            // Must be same with the transmitter
+#define LIGHT_THRESHOLD 300 // Depends on the light intensity of the room
+#define LAMBDA 5 // Must be same with the transmitter
 
-#define STRING_START_CHAR '*'
+bool previous_state;
+bool current_state;
 
-#define THRESHOLD_MULTIPLIER 1.2
-#define DEFAULT_THRESHOLD 300
+int index = 0;
+char buffer[7];
 
-CommunicationHandler comm_handler(LDR_PIN, LAMBDA);
-AdaptiveLight adaptive_light(DEFAULT_THRESHOLD, THRESHOLD_MULTIPLIER);
-
-void setup()
-{
+void setup() {
   Serial.begin(9600);
 }
 
-void loop()
-{
-  const int voltage = comm_handler.read_voltage();
-  adaptive_light.read_light(voltage);
+void loop() {
+  current_state = read_comm();
 
-  const int threshold = adaptive_light.get_adaptive_threshold();
-  comm_handler.read(threshold);
+  if (!current_state && previous_state) {
+    char byte_received = get_byte();
+    // Serial.print("Received byte: ");
+    // Serial.println(byte_received);
+    fill_buffer(byte_received);
+  }
+
+  previous_state = current_state;
+}
+
+bool read_comm() {
+  int voltage = analogRead(LDR_PIN);
+  return voltage > LIGHT_THRESHOLD ? true : false;
+}
+
+char get_byte() {
+  char character = 0;
+  delay(LAMBDA * 1.5);
+  for (int i = 0; i < 8; i++) {
+    character = character | read_comm() << i;
+    delay(LAMBDA);
+  }
+  return character;
+}
+
+void fill_buffer(const char& ch) {
+  if(ch == '*') {
+    index = 0;
+    // Serial.print("Received buffer: ");
+    Serial.println(buffer);
+  } else if(index < 7) {
+    buffer[index] = ch;
+    index++;
+  } else {
+    Serial.println("ERR: Buffer is full");
+  }
 }
