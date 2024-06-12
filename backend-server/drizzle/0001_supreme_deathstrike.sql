2dcc27eadedc8d4ed0bc93ee179f1598c5d9361e
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-06-12 20:54:03 +0300

    Update migration files

--- backend-server/drizzle/0001_supreme_deathstrike.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "sensor_data" ALTER COLUMN "sensor_id" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "sensor_data" ALTER COLUMN "value" SET DATA TYPE smallint;--> statement-breakpoint
-ALTER TABLE "sensor_data" ALTER COLUMN "value" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "sensor_data" ALTER COLUMN "time" SET DATA TYPE bigint;--> statement-breakpoint
-ALTER TABLE "sensor_data" ALTER COLUMN "time" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "sensors" ALTER COLUMN "name" SET NOT NULL;
